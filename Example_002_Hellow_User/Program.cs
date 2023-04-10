ae4a2e713c7a1e3e5a4998b0ce6c2d75f37ba0ed
Вячеслав <chve80@yandex.ru>
2023-04-10 23:33:22 +0300

    Add dir Example_002_Hellow_User

---
+++ Example_002_Hellow_User/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вторая программа. Вывод в консоль приветствия введеного юзера
+Console.WriteLine("Введите ваше имя");
+String username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(username);
